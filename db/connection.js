ccdb53b8e3f9ef5f5b47f43ae798d2add687ccd8
Ayushi Shukla <78807638+Ayushias0203@users.noreply.github.com>
2023-07-20 21:40:25 +0530

    Create connection.js

---
+++ db/connection.js
@@ -0,0 +1,8 @@
+require('dotenv').config();
+const mongoose = require ("mongoose");
+const DB = process.env.REACT_APP_DB_MONGO;
+
+mongoose.connect(DB,{
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(()=>console.log("Connected to database")).catch((error)=>console.log(error.message));
